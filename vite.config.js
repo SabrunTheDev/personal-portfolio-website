fe01567299479640c5646d90f0e4fc12fd4c9037
Sabrun <officialsabrun@gmail.com>
2024-07-28 01:36:58 -0400

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.glb"], // Add this line to include .glb files
+  server: {
+    historyApiFallback: true,
+  },
 });
