ed30edaa0c8cf32815cde9391f78460d05581475
Sabrun <officialsabrun@gmail.com>
2024-07-29 01:05:54 -0400

    Updated files

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,35 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
+
+    {/* Google Analytics setup */}
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${
+        import.meta.env.VITE_APP_PUBLIC_GOOGLE_ANALYTICS
+      }`}
+    ></script>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${
+              import.meta.env.VITE_APP_PUBLIC_GOOGLE_ANALYTICS
+            }', {
+              page_path: window.location.pathname,
+            });
+          `,
+      }}
+    />
   </React.StrictMode>
 );
