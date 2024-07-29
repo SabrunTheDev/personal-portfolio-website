56bb9337bc07fa1a31c612149104e36466e989b3
Sabrun <officialsabrun@gmail.com>
2024-07-29 00:59:04 -0400

    Updated files

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -64,6 +64,29 @@ const Projects = () => {
       <hr className="border-slate-200" />
 
       <CTA />
+
+      {/* Google Analytics setup */}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${
+          import.meta.env.VITE_APP_PUBLIC_GOOGLE_ANALYTICS
+        }`}
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${
+              import.meta.env.VITE_APP_PUBLIC_GOOGLE_ANALYTICS
+            }', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </section>
   );
 };
