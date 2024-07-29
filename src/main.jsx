520d576583614c993bf57ee28af44d8b0b75d51e
Sabrun <officialsabrun@gmail.com>
2024-07-29 01:10:32 -0400

    Updated files

--- src/main.jsx
+++ src/main.jsx
@@ -8,5 +8,28 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Analytics />
+
+    {/* Google Analytics setup */}
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${
+        import.meta.env.VITE_APP_PUBLIC_GOOGLE_ANALYTICS
+      }`}
+    ></script>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${
+              import.meta.env.VITE_APP_PUBLIC_GOOGLE_ANALYTICS
+            }', {
+              page_path: window.location.pathname,
+            });
+          `,
+      }}
+    />
   </React.StrictMode>
 );
